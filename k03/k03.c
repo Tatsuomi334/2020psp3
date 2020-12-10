61380971abd3f539f01b7804f2a5ae96cc56878d
中岡辰浩 <bb35319029@ms.nagasaki-u.ac.jp>
2020-12-10 09:12:05 +0900

    課題③（12.10)

--- k03/k03.c
+++ k03/k03.c
@@ -16,33 +16,33 @@ int text_len;
 int key_len;
 text_len=strlen(text);
 key_len=strlen(key);
-int i=0;int j=0;
-for(start=0; start<text_len-key_len;start++){
-   for(pos=0;pos<key_len-1;pos++){   
-    if(text[start+pos]=key[pos]){
-      if(pos=key_len-1){
+int i=0;int j;
+for(start=0; start<(text_len-key_len);start++){
+   for(pos=0;pos<key_len;pos++){   
+    if(text[start+pos]==key[pos]){
+      if(pos==key_len-1){
       i=1;
       break;
       }
-    else{
-      
     }
-    }
-  
     else{
       break;
     }
+   }
+   if(i==1){
+     break;
+   }
+}
+if(pos==key_len-1){
+  return &text[start];
+}
+else{
+  return NULL;
+}
+}
+  
     
-}
-}
-    if(i==1){
-        return &text[start];
-        
-    }
-     else{
-         return NULL ;  }
-
-}
+    
 
 
 
@@ -65,45 +65,44 @@ char* BMSearch(char text[], char key[])
   while(i<=256){
     table[i]=key_len;
     i++;
-  }
+  } 
   i=0;
   while(i<=key_len){ 
     table[key_len]=(key_len-1)-i;
     i++;
   }
    index=key_len-1;
-  while(index<=text_len){
+  while(index<text_len){
     index_a=index;
     for(key_index=key_len-1;0<=key_index;key_index--){
      if (text[index] == key[key_index]){
        index=index-1;
      
-       if(key_len==0){
+       if(key_index==0){
          x=1;
          break;
+       }
        }
        else{
          index=index+table[text[index]];
          if(index<=index_a){
            index=index_a+1;
          }  
-        
-       }
-    }
-    else{
-      break;
-    }
-    
-  
-  }
-    if(x=1){
-     return &text[index];
+        break;
+       }    
+  } 
+    if(x==1){
+     break;
     }   
+  }
+    if(key_index==0){
+      return &text[index];
+    }
    else
   {
     return NULL;
   }
-  }
+  
    
   
   
